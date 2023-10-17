1c429f61e46359098c14addf4de776057d734f77
Jnnngs <admin@example.com>
2023-10-17 19:36:03 +0100

    Working

--- go3270.go
+++ go3270.go
@@ -1,161 +1,64 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"html"
-	"io"
-	"io/ioutil"
 	"log"
-	"os"
-	"os/exec"
 	"strings"
-	"time"
+
+	go3270 "gitlab.jnnn.gs/jnnngs/go3270/x3270"
 )
 
-type Workflow struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	Login    string `json:"login"`
-	Password string `json:"password"`
-	Steps    []Step `json:"steps"`
-}
-
-type Step struct {
-	Action  string `json:"action"`
-	Message string `json:"message,omitempty"`
-	X       int    `json:"x,omitempty"`
-	Y       int    `json:"y,omitempty"`
-	Data    string `json:"data,omitempty"`
-}
-
-func captureScreen(stdin io.Writer, stdout *bytes.Buffer) string {
-	_, err := stdin.Write([]byte("Ascii()\n"))
-	if err != nil {
-		log.Fatalf("Error writing to stdin: %v", err)
-	}
-	time.Sleep(1 * time.Second) // Giving some time for x3270 to respond
-
-	screenContent := stdout.String()
-	fmt.Println("======= SCREEN CONTENT =======")
-	fmt.Println(screenContent)
-	fmt.Println("==============================")
-	return screenContent
-}
-
-func getConnectionState(stdin io.Writer, stdout *bytes.Buffer) (bool, string) {
-	_, err := stdin.Write([]byte("Query(ConnectionState)\n"))
-	if err != nil {
-		log.Fatalf("Error writing to stdin: %v", err)
-	}
-
-	// Give the command some time to process and for the output to be available
-	time.Sleep(1 * time.Second)
-
-	output := stdout.String()
-
-	// Check if the output contains "ok"
-	return strings.Contains(output, "ok"), output
-}
-
 func main() {
-	data, err := ioutil.ReadFile("config.json")
+	// Create an emulator instance
+	e := go3270.Emulator{
+		Host: "10.27.27.62",
+		Port: 30050,
+	}
+
+	// Connect to the terminal
+	if err := e.Connect(); err != nil {
+		log.Fatalf("Error connecting to terminal: %v\n", err)
+	}
+
+	// Check for successful login
+	v, err := e.GetValue(1, 2, 12) // Adjust the length as needed
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error getting value: %v", err)
 	}
-
-	var workflow Workflow
-	if err := json.Unmarshal(data, &workflow); err != nil {
-		log.Fatal(err)
-	}
-
-	cmd := exec.Command("x3270", "-model", "2", "-script", fmt.Sprintf("%s:%s", workflow.Host, workflow.Port))
-
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stdin.Close()
-
-	time.Sleep(2 * time.Second)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	defer cmd.Process.Kill() // Ensure x3270 process is killed
-
-	connected, state := getConnectionState(stdin, &out)
-	out.Reset() // Reset the buffer after reading
-	if !connected {
-		log.Fatalf("x3270 is not connected. ConnectionState returned: %s", state)
-	}
-
-	htmlFile, err := os.Create("screengrabs.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		htmlFile.Sync()
-		htmlFile.Close()
-	}()
-
-	htmlFile.WriteString("<html><body>")
-
-	//stdin.Write([]byte("Connect(" + workflow.Host + ":" + workflow.Port + ")\n"))
-	//time.Sleep(2 * time.Second) // Wait after connecting
-	//fmt.Println(out.String())   // Print feedback
-
-	for _, step := range workflow.Steps {
-		time.Sleep(2 * time.Second)
-
-		if step.Message != "" {
-			fmt.Println(step.Message) // Display the step message
+	v = strings.TrimSpace(v)             // Remove leading and trailing whitespaces
+	log.Println("Retrieved value: " + v) // Add this line for debugging
+	if v != "Scrn: BANK10" {
+		log.Println("Login failed. String found: --->" + v + "<---")
+		if err := e.Disconnect(); err != nil {
+			log.Fatalf("Error disconnecting: %v", err)
 		}
-
-		switch step.Action {
-		case "string_found":
-			screenContent := captureScreen(stdin, &out) // Capture the current screen
-			positionIndex := (step.Y-1)*80 + step.X     // Convert x,y to index in the content, assuming 80 chars per line.
-			substring := screenContent[positionIndex : positionIndex+len(step.Data)]
-
-			found := substring == step.Data
-
-			fmt.Printf("Looking for string: '%s' at position (%d, %d). Found: %v\n", step.Data, step.X, step.Y, found)
-
-		case "fill_field":
-			cmdStr := fmt.Sprintf("MoveCursor(%d,%d)\n", step.X, step.Y)
-			fmt.Println("Command:", cmdStr) // Display the constructed command
-			stdin.Write([]byte(cmdStr))
-			fmt.Println(out.String())   // Print feedback
-			time.Sleep(3 * time.Second) // Increase the delay after moving the cursor
-
-			cmdStr = fmt.Sprintf("String(%s)\n", step.Data)
-			fmt.Println("Command:", cmdStr) // Display the constructed command
-			stdin.Write([]byte(cmdStr))
-			fmt.Println(out.String()) // Print feedback
-
-		case "send_enter":
-			_, err := stdin.Write([]byte("Enter\n"))
-			if err != nil {
-				log.Fatalf("Error writing to stdin: %v", err)
-			}
-		case "wait":
-			fmt.Printf("Waiting for %d seconds...\n", step.X)
-		case "screengrab":
-			fmt.Println(step.Message)
-			screenContent := captureScreen(stdin, &out)
-			htmlContent := fmt.Sprintf("<pre>%s</pre><hr/>", html.EscapeString(strings.TrimSpace(screenContent)))
-			htmlFile.WriteString(htmlContent)
-		}
-
-		screenContent := captureScreen(stdin, &out)
-		htmlContent := fmt.Sprintf("<pre>%s</pre><hr/>", html.EscapeString(strings.TrimSpace(screenContent)))
-		htmlFile.WriteString(htmlContent)
-		out.Reset() // Reset the buffer after reading
+	} else {
+		log.Println("Successfully connected to BANK10")
 	}
 
-	htmlFile.WriteString("</body></html>")
+	// Move cursor to position and fill in the username
+	//if err := e.MoveCursor(9, 44); err != nil {
+	//	log.Fatalf("Error moving cursor: %v\n", err)
+	//}
+	if err := e.FillString(10, 44, "b0001"); err != nil {
+		log.Fatalf("Error setting username: %v\n", err)
+	}
+
+	// Move cursor to position and fill in the password
+	//if err := e.MoveCursor(11, 44); err != nil {
+	//	log.Fatalf("Error moving cursor: %v\n", err)
+	//}
+	if err := e.FillString(11, 44, "mypass"); err != nil {
+		log.Fatalf("Error setting password: %v\n", err)
+	}
+
+	// Send the Enter key
+	if err := e.Press(go3270.Enter); err != nil {
+		log.Fatalf("Error pressing Enter: %v\n", err)
+	}
+
+	// Disconnect from the terminal
+	if err := e.Disconnect(); err != nil {
+		log.Fatalf("Error disconnecting: %v\n", err)
+	}
+
 }
