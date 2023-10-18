26c7ab98d9ada2e454a5ad42d25f6e7491c28c72
Jnnngs <admin@example.com>
2023-10-18 15:57:27 +0100

    API working

--- go3270.go
+++ go3270.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
 	go3270 "gitlab.jnnn.gs/jnnngs/go3270/x3270"
+
+	"github.com/gin-gonic/gin"
 )
 
 // Configuration struct to hold the configuration settings
@@ -26,24 +30,36 @@ type Step struct {
 
 var (
 	configFile string
-	showHelp   bool // Define a flag for help
+	showHelp   bool
+	runAPI     bool
+	apiPort    int
 )
 
 func init() {
 	flag.StringVar(&configFile, "config", "workflow.json", "Path to the configuration file")
-	flag.BoolVar(&showHelp, "help", false, "Show usage information") // Add the help flag
+	flag.BoolVar(&showHelp, "help", false, "Show usage information")
+	flag.BoolVar(&runAPI, "api", false, "Run as API")
+	flag.IntVar(&apiPort, "api-port", 8080, "API port")
 }
 
 func main() {
-	// Parse command-line flags
 	flag.Parse()
 
-	// Display usage information and exit if -help is provided
 	if showHelp {
 		flag.Usage()
 		return
 	}
 
+	if runAPI {
+		// Run as an API
+		runAPIWorkflow()
+	} else {
+		// Run as CLI
+		runCLIWorkflow()
+	}
+}
+
+func runCLIWorkflow() {
 	// Read the configuration from the external JSON file
 	configFile, err := os.Open(configFile)
 	if err != nil {
@@ -119,4 +135,87 @@ func main() {
 			log.Printf("Unknown step type: %s\n", step.Type)
 		}
 	}
+
+}
+
+func runAPIWorkflow() {
+	r := gin.Default()
+
+	r.POST("/api/execute", func(c *gin.Context) {
+		// Read the JSON payload from the request body
+		var workflowConfig Configuration
+		if err := c.ShouldBindJSON(&workflowConfig); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Create an emulator instance
+		e := go3270.Emulator{
+			Host: workflowConfig.Host,
+			Port: workflowConfig.Port,
+		}
+
+		// Initialize the HTML file with run details (call this at the beginning)
+		htmlFilePath := workflowConfig.HTMLFilePath
+		if err := e.InitializeHTMLFile(htmlFilePath); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Iterate through the steps in the configuration
+		for _, step := range workflowConfig.Steps {
+			switch step.Type {
+			case "InitializeHTMLFile":
+				if err := e.InitializeHTMLFile(htmlFilePath); err != nil {
+					log.Fatalf("Error initializing HTML file: %v\n", err)
+				}
+			case "Connect":
+				if err := e.Connect(); err != nil {
+					log.Fatalf("Error connecting to terminal: %v\n", err)
+				}
+			case "CheckValue":
+				//log.Printf("step.Coordinates.Row value: %d", step.Coordinates.Row)
+				//log.Printf("step.Coordinates.Column value: %d", step.Coordinates.Column)
+				//log.Printf("step.Length value: %d", step.Coordinates.Length)
+
+				v, err := e.GetValue(step.Coordinates.Row, step.Coordinates.Column, step.Coordinates.Length)
+				if err != nil {
+					log.Fatalf("Error getting value: %v", err)
+				}
+				v = strings.TrimSpace(v)
+				log.Println("Retrieved value: " + v)
+				if v != step.Text {
+					log.Printf("Login failed. Expected: %s, Found: %s\n", step.Text, v)
+					if err := e.Disconnect(); err != nil {
+						log.Fatalf("Error disconnecting: %v", err)
+					}
+					return
+				}
+			case "FillString":
+				if err := e.FillString(step.Coordinates.Row, step.Coordinates.Column, step.Text); err != nil {
+					log.Fatalf("Error setting text: %v\n", err)
+				}
+			case "AsciiScreenGrab":
+				if err := e.AsciiScreenGrab(htmlFilePath, true); err != nil {
+					log.Fatalf("Error capturing and appending ASCII screen: %v", err)
+				}
+			case "PressEnter":
+				if err := e.Press(go3270.Enter); err != nil {
+					log.Fatalf("Error pressing Enter: %v\n", err)
+				}
+			case "Disconnect":
+				if err := e.Disconnect(); err != nil {
+					log.Fatalf("Error disconnecting: %v\n", err)
+				}
+			default:
+				log.Printf("Unknown step type: %s\n", step.Type)
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Workflow executed successfully"})
+	})
+
+	apiAddr := fmt.Sprintf(":%d", apiPort)
+	log.Printf("API server is running on %s", apiAddr)
+	r.Run(apiAddr)
 }
