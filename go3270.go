c965c6054b52455f9ee167e5a62d61f4c12b6cb0
Jnnngs <admin@example.com>
2023-10-18 07:57:14 +0100

    Working using workflow.json

--- go3270.go
+++ go3270.go
@@ -1,83 +1,102 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 	"strings"
 
 	go3270 "gitlab.jnnn.gs/jnnngs/go3270/x3270"
 )
 
-// Global variable for the HTML file path
-var filePath = "output.html"
+// Configuration struct to hold the configuration settings
+type Configuration struct {
+	Host         string
+	Port         int
+	HTMLFilePath string `json:"HTMLFilePath"`
+	Steps        []Step
+}
+
+type Step struct {
+	Type        string
+	Coordinates go3270.Coordinates // Use go3270 package's Coordinates type
+	Text        string
+}
 
 func main() {
+	// Read the configuration from the external JSON file
+	configFile, err := os.Open("workflow.json")
+	if err != nil {
+		log.Fatalf("Error opening config file: %v", err)
+	}
+	defer configFile.Close()
+
+	var config Configuration
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalf("Error decoding config JSON: %v", err)
+	}
+
 	// Create an emulator instance
 	e := go3270.Emulator{
-		Host: "10.27.27.62",
-		Port: 30050,
+		Host: config.Host,
+		Port: config.Port,
 	}
 
 	// Initialize the HTML file with run details (call this at the beginning)
-	htmlFilePath := "output.html"
+	htmlFilePath := config.HTMLFilePath
 	if err := e.InitializeHTMLFile(htmlFilePath); err != nil {
 		log.Fatalf("Error initializing HTML file: %v\n", err)
 	}
 
-	// Connect to the terminal
-	if err := e.Connect(); err != nil {
-		log.Fatalf("Error connecting to terminal: %v\n", err)
-	}
+	// Iterate through the steps in the configuration
+	for _, step := range config.Steps {
+		switch step.Type {
+		case "InitializeHTMLFile":
+			if err := e.InitializeHTMLFile(htmlFilePath); err != nil {
+				log.Fatalf("Error initializing HTML file: %v\n", err)
+			}
+		case "Connect":
+			if err := e.Connect(); err != nil {
+				log.Fatalf("Error connecting to terminal: %v\n", err)
+			}
+		case "CheckValue":
+			//log.Printf("step.Coordinates.Row value: %d", step.Coordinates.Row)
+			//log.Printf("step.Coordinates.Column value: %d", step.Coordinates.Column)
+			//log.Printf("step.Length value: %d", step.Coordinates.Length)
 
-	// Check for successful login
-	v, err := e.GetValue(1, 2, 12) // Adjust the length as needed
-	if err != nil {
-		log.Fatalf("Error getting value: %v", err)
-	}
-	v = strings.TrimSpace(v)             // Remove leading and trailing whitespaces
-	log.Println("Retrieved value: " + v) // Add this line for debugging
-	if v != "Scrn: BANK10" {
-		log.Println("Login failed. String found: --->" + v + "<---")
-		if err := e.Disconnect(); err != nil {
-			log.Fatalf("Error disconnecting: %v", err)
+			v, err := e.GetValue(step.Coordinates.Row, step.Coordinates.Column, step.Coordinates.Length)
+			if err != nil {
+				log.Fatalf("Error getting value: %v", err)
+			}
+			v = strings.TrimSpace(v)
+			log.Println("Retrieved value: " + v)
+			if v != step.Text {
+				log.Printf("Login failed. Expected: %s, Found: %s\n", step.Text, v)
+				if err := e.Disconnect(); err != nil {
+					log.Fatalf("Error disconnecting: %v", err)
+				}
+				return
+			}
+		case "FillString":
+			if err := e.FillString(step.Coordinates.Row, step.Coordinates.Column, step.Text); err != nil {
+				log.Fatalf("Error setting text: %v\n", err)
+			}
+		case "AsciiScreenGrab":
+			if err := e.AsciiScreenGrab(htmlFilePath, true); err != nil {
+				log.Fatalf("Error capturing and appending ASCII screen: %v", err)
+			}
+		case "PressEnter":
+			if err := e.Press(go3270.Enter); err != nil {
+				log.Fatalf("Error pressing Enter: %v\n", err)
+			}
+		case "Disconnect":
+			if err := e.Disconnect(); err != nil {
+				log.Fatalf("Error disconnecting: %v\n", err)
+			}
+		default:
+			log.Printf("Unknown step type: %s\n", step.Type)
 		}
-	} else {
-		log.Println("Successfully connected to BANK10")
 	}
-
-	// Move cursor to position and fill in the username
-	//if err := e.MoveCursor(9, 44); err != nil {
-	//	log.Fatalf("Error moving cursor: %v\n", err)
-	//}
-	if err := e.FillString(10, 44, "b0001"); err != nil {
-		log.Fatalf("Error setting username: %v\n", err)
-	}
-
-	// Move cursor to position and fill in the password
-	//if err := e.MoveCursor(11, 44); err != nil {
-	//	log.Fatalf("Error moving cursor: %v\n", err)
-	//}
-	if err := e.FillString(11, 44, "mypass"); err != nil {
-		log.Fatalf("Error setting password: %v\n", err)
-	}
-
-	// Capture and append the ASCII screen to the HTML file
-	if err := e.AsciiScreenGrab(htmlFilePath, true); err != nil {
-		log.Fatalf("Error capturing and appending ASCII screen: %v", err)
-	}
-
-	// Send the Enter key
-	if err := e.Press(go3270.Enter); err != nil {
-		log.Fatalf("Error pressing Enter: %v\n", err)
-	}
-
-	// Capture and append the ASCII screen to the HTML file
-	if err := e.AsciiScreenGrab(htmlFilePath, true); err != nil {
-		log.Fatalf("Error capturing and appending ASCII screen: %v", err)
-	}
-
-	// Disconnect from the terminal
-	if err := e.Disconnect(); err != nil {
-		log.Fatalf("Error disconnecting: %v\n", err)
-	}
-
 }
