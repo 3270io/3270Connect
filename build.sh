af0a7954bfb5909ba71a214f02c2685f304cce77
jnnngs <4107228+jnnngs@users.noreply.github.com>
2024-06-18 12:35:20 +0100

    Jenkinsfile

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 docker build --target final-linux -t 3270connect-linux .
 
 # Build the Windows image
-#docker build --target final-windows -t 3270connect-windows .
+docker build --target final-windows -t 3270connect-windows .
